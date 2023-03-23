cfea65f8f0ab3edb1c323b0986312a16230f64be
satishusk <sanpavkaz@gmail.com>
2023-03-23 14:19:19 +0400

    Init project

---
+++ src/main/java/com/example/lab3/Lab3Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3;
+
+import com.example.lab3.client.FtpClientWrapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Lab3Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Lab3Application.class, args);
+  }
+}

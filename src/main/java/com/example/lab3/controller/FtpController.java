4c4d5d127831e957662cdc31ee3def61b371df1c
satishusk <sanpavkaz@gmail.com>
2023-03-23 14:21:26 +0400

    Added controller

---
+++ src/main/java/com/example/lab3/controller/FtpController.java
@@ -0,0 +1,45 @@
+package com.example.lab3.controller;
+
+import com.example.lab3.client.FtpClientWrapper;
+import com.example.lab3.service.FtpCatalogueTreeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class FtpController {
+  private final FtpClientWrapper ftpClientWrapper;
+  private final FtpCatalogueTreeService treeService;
+
+  @GetMapping("/tree")
+  public String filesTreePage() {
+    return "tree";
+  }
+
+  @PostMapping("/tree")
+  public String filesTree(
+    @RequestParam("hostname") String hostname, @RequestParam("port") Integer port,
+    @RequestParam("username") String username, @RequestParam("password") String password,
+    Model model
+  ) throws IOException {
+    ftpClientWrapper.connect(hostname, port, username, password);
+
+    Map<String, Object> attributes = Map.of(
+      "hostname", hostname,
+      "port", port,
+      "username", username,
+      "directory", treeService.listDirectory(ftpClientWrapper.getFtpClient())
+    );
+    model.addAllAttributes(attributes);
+
+    return "tree";
+  }
+}

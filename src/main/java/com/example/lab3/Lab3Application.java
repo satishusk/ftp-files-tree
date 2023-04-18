8746475986fe162369e7c6cdfd6d235d342647bb
satishusk <sanpavkaz@gmail.com>
2023-04-18 20:34:52 +0400

    Some manual refactoring

--- src/main/java/com/example/lab3/Lab3Application.java
+++ src/main/java/com/example/lab3/Lab3Application.java
@@ -1,9 +1,7 @@
 package com.example.lab3;
 
-import com.example.lab3.client.FtpClientWrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lab3Application {

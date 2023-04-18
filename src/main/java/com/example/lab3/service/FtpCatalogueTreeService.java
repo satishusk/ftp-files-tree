9dcb53e94e63573df2a6aed8767bf4cb246b91c9
satishusk <sanpavkaz@gmail.com>
2023-04-18 20:34:14 +0400

    Reworked FtpCatalogueTreeService to new design

--- src/main/java/com/example/lab3/service/FtpCatalogueTreeService.java
+++ src/main/java/com/example/lab3/service/FtpCatalogueTreeService.java
@@ -1,44 +1,39 @@
 package com.example.lab3.service;
 
+import com.example.lab3.client.FtpClient;
+import com.example.lab3.model.FtpFile;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FtpCatalogueTreeService {
-  public List<String> listDirectory(FTPClient ftpClient) throws IOException {
+  public List<String> listDirectory(FtpClient ftpClient) {
     var list = new ArrayList<String>();
-    listDirectoryRecursive(ftpClient, "", ".", 0, list);
+    listDirectoryRecursive(ftpClient, "/", ".", 0, list);
     return list;
   }
 
-  public List<String> listDirectory(FTPClient ftpClient, String parentDir, String currentDir, int level) throws IOException {
-    var list = new ArrayList<String>();
-    listDirectoryRecursive(ftpClient, parentDir, currentDir, level, list);
-    return list;
-  }
-
-  private void listDirectoryRecursive(
-    FTPClient client, String parentDir, String currentDir, int level, List<String> list
-  ) throws IOException {
+  private void listDirectoryRecursive(FtpClient client,
+                                      String parentDir,
+                                      String currentDir,
+                                      int level,
+                                      List<String> list) {
     String dirToList = parentDir;
-    if (!currentDir.equals("")) {
-      dirToList += "/" + currentDir;
-    }
+    dirToList += currentDir + "/";
 
-    FTPFile[] subFiles = client.listFiles(dirToList);
-    if (subFiles == null || subFiles.length == 0){
+    List<FtpFile> subFiles = client.listFiles(dirToList);
+    if (subFiles == null || subFiles.size() == 0){
       return;
     }
 
-    for (FTPFile aFile : subFiles) {
-      String currentFileName = new String(aFile.getName().getBytes(Charset.forName("Cp1251")));
+    for (FtpFile aFile : subFiles) {
+      String currentFileName = aFile.getName();
       if (currentFileName.equals(".") || currentFileName.equals("..")) {
         continue;
       }

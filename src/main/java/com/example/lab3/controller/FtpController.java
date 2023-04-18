47b8195090535c305ea415c8adebd9145c6be0ba
satishusk <sanpavkaz@gmail.com>
2023-04-18 20:35:21 +0400

    Reworked FtpController to new design

--- src/main/java/com/example/lab3/controller/FtpController.java
+++ src/main/java/com/example/lab3/controller/FtpController.java
@@ -1,45 +1,60 @@
 package com.example.lab3.controller;
 
-import com.example.lab3.client.FtpClientWrapper;
+import com.example.lab3.client.FtpClient;
+import com.example.lab3.model.FtpAccount;
 import com.example.lab3.service.FtpCatalogueTreeService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Controller
-@RequiredArgsConstructor
 public class FtpController {
-  private final FtpClientWrapper ftpClientWrapper;
   private final FtpCatalogueTreeService treeService;
+  private final Map<String, Object> ftpClientAttributes;
+
+  public FtpController(FtpCatalogueTreeService treeService, ApplicationContext context) {
+    this.treeService = treeService;
+    this.ftpClientAttributes = FtpAccount.byProfile(context).toMap();
+  }
 
   @GetMapping("/tree")
-  public String filesTreePage() {
+  public String filesTreePage(Model model) {
+    if (ftpClientAttributes.size() > 0) {
+      model.addAllAttributes(ftpClientAttributes);
+    }
     return "tree";
   }
 
   @PostMapping("/tree")
-  public String filesTree(
-    @RequestParam("hostname") String hostname, @RequestParam("port") String port,
-    @RequestParam("username") String username, @RequestParam("password") String password,
-    Model model
-  ) throws IOException {
-    ftpClientWrapper.connect(hostname, Integer.parseInt(port), username, password);
-
+  public String filesTree(@RequestParam("hostname") String hostname,
+                          @RequestParam("port") String port,
+                          @RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          Model model) throws IOException {
+    FtpClient ftpClient = new FtpClient(hostname, Integer.parseInt(port), username, password);
     Map<String, Object> attributes = Map.of(
       "hostname", hostname,
       "port", port,
       "username", username,
-      "directory", treeService.listDirectory(ftpClientWrapper.getFtpClient())
+      "password", password,
+      "directory", treeService.listDirectory(ftpClient)
     );
     model.addAllAttributes(attributes);
 
     return "tree";
+  }
+
+  @ExceptionHandler(IOException.class)
+  public String handle(Model model, IOException exception) {
+    exception.printStackTrace();
+    model.addAttribute("error", exception.getMessage());
+    return filesTreePage(model);
   }
 }
